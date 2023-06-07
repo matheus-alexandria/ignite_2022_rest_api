ca35f676093b9a3ce2d0e45e1edf08bd594a30f9
Matchobas <maalexandria@hotmail.com>
2023-06-06 21:33:55 -0300

    Configuring knex and creating first test request

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify';
+import { knex } from './database';
 
 const server = fastify();
+
+server.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*');
+
+  return tables;
+});
 
 server.listen({ port: 3333 }, () => {
   console.log('HTTP Server Online!');
