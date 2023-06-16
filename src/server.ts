cf2283c557d12e0393f9afcf8e51eac755b709b8
Matchobas <maalexandria@hotmail.com>
2023-06-16 00:21:01 -0300

    Creating firt knex queries

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const server = fastify();
 
 server.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transação Teste',
+    amount: 1000,
+  }).returning('*');
 
-  return tables;
+  return transaction;
 });
 
 server.listen({ port: 3333 }, () => {
