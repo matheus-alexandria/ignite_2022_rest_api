32d7d0cbdc92ab1556b716f776da8026950b83ec
Matchobas <maalexandria@hotmail.com>
2023-06-16 00:28:55 -0300

    Adding envs with dotenv

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,9 @@
+import 'dotenv/config';
 import { knex as setupKnex, Knex } from 'knex';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Database url env not found');
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
