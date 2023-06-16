b0c480a36b31ff161e9fe49ecf35a99fe3e03ac3
Matchobas <maalexandria@hotmail.com>
2023-06-16 14:33:53 -0300

    Change zod warning error and creating more env vars

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,8 +2,17 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 });
 
-export const env = envSchema.parse(process.env);
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (parsedEnv.success === false) {
+  console.error('Invalid enviroment variables!', parsedEnv.error.format());
+
+  throw new Error('Invalid env variables.');
+}
+
+export const env = parsedEnv.data;
