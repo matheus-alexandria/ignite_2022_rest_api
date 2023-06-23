3297a07ef418f68193c1412cbcc6409cffcba684
Matchobas <maalexandria@hotmail.com>
2023-06-23 11:05:04 -0300

    Put transactions routes inside test describe

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -1,25 +1,27 @@
 import {
-  afterAll, beforeAll, expect, test,
+  afterAll, beforeAll, expect, describe, it,
 } from 'vitest';
 import request from 'supertest';
 import { server } from '../initFastifyServer';
 
-beforeAll(async () => {
-  await server.ready();
-});
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await server.ready();
+  });
 
-afterAll(async () => {
-  await server.close();
-});
+  afterAll(async () => {
+    await server.close();
+  });
 
-test('User must be able to create a new transaction', async () => {
-  const response = await request(server.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    });
+  it('should be able to create a new transaction', async () => {
+    const response = await request(server.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      });
 
-  expect(response.statusCode).toEqual(201);
+    expect(response.statusCode).toEqual(201);
+  });
 });
